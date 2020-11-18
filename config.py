4c9b19f23fa4de0073d6231b12a725bc538e0b6c
Darknessest <darknessest>
2020-11-18 16:41:51 +0800

    [again] Added random timeout before filling in

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-ifttt_key = 'dtFZmutEg9ANUtSEpAU7Tu'                # if you don't have it, program will still work
-login = ['24320172205050', '26520209772072']
-password = ['829492', '561710']
+ifttt_key = ''                # if you don't have it, program will still work
+login = ['', '']
+password = ['', '']
 sleep_time = [2, 29]                                # range from first to second in minutes
