8d35444949ad11485a40b430a616ff9f952862d2
Darknessest <darknessest>
2020-11-18 16:41:12 +0800

    Added random timeout before filling in

--- daily_health_report.py
+++ daily_health_report.py
@@ -1,10 +1,18 @@
-from config import login, password
+from random import randint
+from time import sleep
+
+from config import login, password, sleep_time
 from fill_in import fill_in
 
 '''
     FILL IN
 '''
 for i in range(len(login)):
+    # random timeout before filling in, check config file for settings
+    timeout = randint(sleep_time[0] * 60, sleep_time[1] * 60)
+    print('timeout is =', timeout)
+    sleep(timeout)
+
     if login[i] != '' and password[i] != '':
         fill_in(login[i], password[i])
     else:
